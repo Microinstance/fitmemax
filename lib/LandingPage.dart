d2276a6a19ddf70f8c2fc5743f303d7e7a3008ed
Shivay <shivay.paradox@gmail.com>
2020-11-30 12:12:32 +0530

    done

--- lib/LandingPage.dart
+++ lib/LandingPage.dart
@@ -1,10 +1,5 @@
 import 'package:fitmemax/src/module/dashboard/Dashboard.dart';
-import 'package:fitmemax/src/module/dashboard/profile/Profile.dart';
-import 'package:fitmemax/src/module/dashboard/profile/ProfileHome.dart';
 import 'package:flutter/material.dart';
-
-import 'dummy.dart';
-
 class LandingPage extends StatefulWidget {
   @override
   _LandingPageState createState() => _LandingPageState();
