b4d710ade183c6831ee3e9d7eb3a94eb6ca95e7e
ParthDomadiya <parthdomadiya21@gmail.com>
2021-02-13 12:02:33 +0530

    Add Api

--- lib/ApiProvider/apiProvider.dart
+++ lib/ApiProvider/apiProvider.dart
@@ -34,8 +34,8 @@ class ApiProvider {
   Future userRegistration(String email, String name, String password, String mobile) async {
     var result;
     try {
-      // var urlString = ConstanceData.BaseApiUrl + "user/user-registration";
-      var urlString = "http://project.neutronvision.com/fitBook/public/api/v1/user/user-registration";
+      var urlString = ConstanceData.BaseApiUrl + "user/user-registration";
+      // var urlString = "http://project.neutronvision.com/fitBook/public/api/v1/user/user-registration";
       var response = await http.post(
         Uri.encodeFull(urlString),
         headers: {
@@ -270,6 +270,114 @@ class ApiProvider {
           "Accept": "application/json",
         },
       );
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data != null) {
+          result = data;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  // Slider Detail
+  Future getsliders() async {
+    var result;
+    try {
+      var urlString = ConstanceData.BaseApiUrl + "get-sliders";
+      var response = await http.get(Uri.encodeFull(urlString), headers: {
+        "Accept": "application/json",
+      });
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data != null) {
+          result = data;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  // Categories Detail
+  Future getCategories() async {
+    var result;
+    try {
+      var urlString = ConstanceData.BaseApiUrl + "get-categories";
+      var response = await http.get(Uri.encodeFull(urlString), headers: {
+        "Accept": "application/json",
+      });
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data != null) {
+          result = data;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  Future getSubCategories(String categoryId) async {
+    var result;
+    try {
+      var urlString = ConstanceData.BaseApiUrl + "get-sub-categories" + categoryId;
+      var response = await http.get(Uri.encodeFull(urlString), headers: {
+        "Accept": "application/json",
+      });
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data != null) {
+          result = data;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  // Blog Detail
+  Future getBlogs() async {
+    var result;
+    try {
+      var urlString = ConstanceData.BaseApiUrl + "get-blogs";
+      var response = await http.get(Uri.encodeFull(urlString), headers: {
+        "Accept": "application/json",
+      });
+      if (response.statusCode == 200) {
+        var data = jsonDecode(response.body);
+        if (data != null) {
+          result = data;
+        }
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return result;
+  }
+
+  Future getBlogDetail(String blogId) async {
+    var result;
+    try {
+      var urlString = ConstanceData.BaseApiUrl + "blog-details" + blogId;
+      var response = await http.get(Uri.encodeFull(urlString), headers: {
+        "Accept": "application/json",
+      });
       if (response.statusCode == 200) {
         var data = jsonDecode(response.body);
         if (data != null) {
